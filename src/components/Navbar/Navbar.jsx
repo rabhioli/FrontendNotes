d2d7152a34fe685b3ec781e99887692a0bb46dd8
rabhi alqadi <rabhialqadi22@gmail.com>
2024-05-02 21:35:41 -0700

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
+import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 
-
 const Navbar = () => {
+const navigate = useNavigate();
+
+const onLogout = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
+
     return (
         <div className='bg-white flex items-center justify-between px-6 py-2 drop-shadow'>
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+        <ProfileInfo onLogout={onLogout} />
         </div>
     );
 }
