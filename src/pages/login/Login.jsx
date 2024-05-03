64729924e397cd0abfc89703b249397bc7d38a9b
rabhi alqadi <rabhialqadi22@gmail.com>
2024-05-02 20:24:36 -0700

    password

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from "react";
 import Navbar from '../../components/Navbar';
 import { Link } from 'react-router-dom';
+import PasswordInput from "../../components/Input/PasswordInput";
 
 const Login = () => {
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+        const [error, setError] = useState(null);
+      
+
     return (
         <>
             <Navbar />
@@ -12,6 +18,7 @@ const Login = () => {
                     <form onSubmit={() => {}}>
                         <h4 className='text-2xl mb-7'>Login </h4>
                         <input type='text' placeholder='Email' className='input-box' />
+                        <PasswordInput />
                         <button type='submit' className='btn-primary'>
                             Login
                         </button>
